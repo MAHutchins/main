cf7a777c72292a5f7ffecba8275827681006ba8c
HOCProj <HOCProj@gmail.com>
2017-03-16 00:00:08 -0500

    Create GitAll.py

---
+++ GitAll.py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+
+#Git all of the files for the ADC and Twitter
+
+#Git MCP3008 files
+#Guide here:
+#https://learn.adafruit.com/raspberry-pi-analog-to-digital-converters/mcp3008
+print(git MCP3008 Adafruit library...)
+sudo apt-get install build-essential python-dev python-smbus git
+cd ~
+git clone https://github.com/adafruit/Adafruit_Python_MCP3008.git
+print(got MCP3008 Adafruit library!)
+
+#Git Twython files
+#Guide here:
+#https://www.raspberrypi.org/learning/getting-started-with-the-twitter-api/work$
+print(git Twython...)
+sudo pip3 install twython
+print(got Twython!)
+
+#Git authentication and boot files
+sudo git clone https://github.com/HOCProj/main/ RaspberryPi/auth.py
+print(got auth.py!)
+sudo git clone https://github.com/HOCProj/main/ RaspberryPi/boot.py
+print(got boot.py!)
