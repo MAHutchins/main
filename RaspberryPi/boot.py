9998887a607d449411e9f32817d58ca961f5cb3f
HOCProj <HOCProj@gmail.com>
2017-03-15 23:39:39 -0500

    Create boot.py

---
+++ RaspberryPi/boot.py
@@ -0,0 +1,25 @@
+#!usr/bin/python
+print("Boot script started")
+
+import random
+import datetime
+from twython import Twython
+print("Twython imported successfully.")
+#auth.py is a copy of auth.py in /home/pi/twitter
+from auth import (
+        conKey,
+        conSec,
+        accTok,
+        accSec
+)
+print("API info imported successfully.")
+
+n = random.uniform(0,10)
+t = datetime.datetime.now()
+tweetStr = t.strftime('%d/%m/%Y %H:%M:%S ') + str(n)
+
+print("Tweet is " + tweetStr)
+print("Sending boot tweet...")
+api=Twython(conKey,conSec,accTok,accSec)
+api.update_status(status=tweetStr)
+print("Boot tweet sent successfully")
