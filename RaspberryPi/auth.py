5fd008421b9b6e6dfbb998d9aaea9a1a4fef8a5f
HOCProj <HOCProj@gmail.com>
2017-03-15 23:40:06 -0500

    Create auth.py

---
+++ RaspberryPi/auth.py
@@ -0,0 +1,4 @@
+conKey='GCLlUyGvXTIjRELWRYnjyelS7'
+conSec='9lBLKRcssiMtqA5GCtSoDISwA4aem8wFIeC7kBn5FCcoCFj30M'
+accTok='839992527637884929-qigQOgQeS5oulACyGBLo8cDmX7hXfua'
+accSec='FGPwt1ZoM8eWhI7MUhECfAxGcsrwmpWSfbBy0B2nBtLsF'
