f6257cd3a60c3edbb65d0c5c39aec5c818e09af9
HOCProj <HOCProj@gmail.com>
2017-04-10 10:51:53 -0500

    Update auth.py

--- RaspberryPi/auth.py
+++ RaspberryPi/auth.py
@@ -1,4 +1,5 @@
 #This is the file in which all of your Twitter API authentation information is stored
+#!usr/bin/python
 conKey='GCLlUyGvXTIjRELWRYnjyelS7'
 conSec='9lBLKRcssiMtqA5GCtSoDISwA4aem8wFIeC7kBn5FCcoCFj30M'
 accTok='839992527637884929-qigQOgQeS5oulACyGBLo8cDmX7hXfua'
